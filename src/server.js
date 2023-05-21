e6e2c7f4c5a15472bc91600b8caa099ae28995fe
Kim-Yeon-joo <asd7725@naver.com>
2023-05-21 14:35:27 +0900

    1.1

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 import http from "http";
 import express from "express"; 
 import SocketIO from "socket.io";
@@ -10,16 +11,17 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home")); 
 app.get("/*", (req, res) => res.redirect("/"));
 
-const httpServer = http.createServer(app); 
+const httpServer = http.createServer(app); // 서버 생성
 
-const wsServer = SocketIO(httpServer);
+const wsServer = SocketIO(httpServer); // 소켓 서버랑 합치기
 
+// 소켓 연결 시
 wsServer.on("connection", (socket) => {
     socket.on("join_room", (roomName) => {
         socket.join(roomName);
         socket.to(roomName).emit("welcome");
     });
-    socket.on("offer", (offer, roomName) => {
+    socket.on("offer", (offer, roomName) => { // offer가 들어오면 roomName에 있는 사람들에게 offer를 전송
         socket.to(roomName).emit("offer", offer);
     });
     socket.on("answer", (answer, roomName) => {
