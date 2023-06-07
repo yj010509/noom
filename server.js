370a4bf79e18ac61c6d8ca25c5e79f6645c55904
Kim-Yeon-joo <asd7725@naver.com>
2023-06-08 03:50:43 +0900

    1.6

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from "express";
 // import WebSocket from "ws";
-import SocketIO from "socket.io";
+import {Server} from "socket.io";
 import http from "http";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set("views", process.cwd() + "/views");
 app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
 
-app.use("/public", express.static(process.cwd() + "/src/public"));
+app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -21,7 +21,7 @@ app.get("/*", (req, res) => {
 });
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer);
 
 let roomObjArr = [
   // {
